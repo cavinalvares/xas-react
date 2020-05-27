91ad9b2d71481157ad3710796eb52a326778a46d
cavinalvares <55971040+cavinalvares@users.noreply.github.com>
2020-05-27 22:50:24 +0530

    Update route.js

--- src/components/route.js
+++ src/components/route.js
@@ -92,8 +92,8 @@ export default class Car extends React.Component {
 	   <tr align="center"><th width="800"><h2>Combination</h2></th><th><h2>recommended</h2></th></tr>
           {this.state.persons.map((c)=>{
             return(
-            <tr key = {c.id} ><td width="5%" style={{textAlign:"center"}} className="text-black"><h5>{c.name}</h5></td>
-			      <td width="5%" style={{textAlign:"center"}} className="text-black"><h5>{c.r}</h5></td></tr>
+            <tr key = {c.id} ><td width="800" style={{textAlign:"center"}} className="text-black"><h5>{c.name}</h5></td>
+			      <td width="800" style={{textAlign:"center"}} className="text-black"><h5>{c.r}</h5></td></tr>
             )
           })}
         </tbody>
