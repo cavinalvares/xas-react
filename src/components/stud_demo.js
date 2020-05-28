37bfc10564fc16065cbf63c4aa491c502e9e1637
cavinalvares <55971040+cavinalvares@users.noreply.github.com>
2020-05-28 15:03:24 +0530

    Update stud_demo.js

--- src/components/stud_demo.js
+++ src/components/stud_demo.js
@@ -119,7 +119,7 @@ class Stud_demo extends React.Component {
   
 
     
-	<div class="site-blocks-cover  overlay" style={{backgroundImage:"url(./images/students.jpg)",backgroundSize:"100% 100%"}} data-aos="fade" data-stellar-background-ratio="0.5">
+	<div class="  my-overlay" style={{backgroundImage:"url(./images/students.jpg)",backgroundSize:"100% 100%"}} data-aos="fade">
 <div class="container">
         <div class="row align-items-center justify-content-center text-center">
 
