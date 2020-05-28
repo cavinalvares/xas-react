9853e94761ef69ca9ef23c0022090a4fb3d88066
cavinalvares <55971040+cavinalvares@users.noreply.github.com>
2020-05-28 21:00:57 +0530

    Update form_arts.js

--- src/components/form_arts.js
+++ src/components/form_arts.js
@@ -213,7 +213,7 @@ export default class Form_arts extends React.Component {
       
     </header>
 	
-	   <div class="site-blocks-cover  overlay" style={{backgroundImage:"url(./images/arts_add.jpg)"}} data-aos="fade" data-stellar-background-ratio="0.5">
+	   <div class="  my-overlay" style={{backgroundImage:"url(./images/arts_add.jpg)",backgroundSize:"100% 100%"}} data-aos="fade">
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
 
