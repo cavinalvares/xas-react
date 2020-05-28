e91863ff9e2e394dc0fb62c618dd505d7ead83cf
cavinalvares <55971040+cavinalvares@users.noreply.github.com>
2020-05-28 21:09:26 +0530

    Update stud_desk.js

--- src/components/stud_desk.js
+++ src/components/stud_desk.js
@@ -75,7 +75,7 @@ class Stud_desk extends React.Component {
       
     </header>
 
-<div class="site-blocks-cover  overlay" style={{backgroundImage:"url(./images/students_desk.jpg)"}} data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="  my-overlay" style={{backgroundImage:"url(./images/students_desk.jpg)",backgroundSize:"100% 100%"}} data-aos="fade">
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
 		
