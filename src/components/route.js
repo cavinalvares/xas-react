348c5ec94930afb0453d1ce2b66b5de4d7d8b2fb
cavinalvares <55971040+cavinalvares@users.noreply.github.com>
2020-05-28 21:06:23 +0530

    Update route.js

--- src/components/route.js
+++ src/components/route.js
@@ -78,7 +78,7 @@ export default class Car extends React.Component {
     </header>
 
 	        
-<div class="  my-overlay" style={{backgroundImage:"url(./images/teachers.jpg)",backgroundSize:"100% 100%"}} data-aos="fade">
+<div class="  my-overlay" style={{backgroundImage:"url(./images/science_add.jpg)",backgroundSize:"100% 100%"}} data-aos="fade">
       <div className="container">
         <div className="row align-items-center justify-content-center text-center">
 
